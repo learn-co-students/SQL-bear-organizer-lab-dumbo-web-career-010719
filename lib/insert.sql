f36e40d40181c82f3b3817c8dd71eebe64551d67
edwinlin <edwinlin@gmail.com>
2019-01-13 17:16:13 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age) VALUES ('Mr. Chocolate', 10);
+INSERT INTO bears (name, age) VALUES ('Rowdy', 2);
+INSERT INTO bears (name, age) VALUES ('Tabitha', 1);
+INSERT INTO bears (name, age) VALUES ('Sergeant Brown', 5);
+INSERT INTO bears (name, age) VALUES ('Melissa', 11);
+INSERT INTO bears (name, age) VALUES ('Grinch', 4);
+INSERT INTO bears (name, age) VALUES ('Wendy', 4);
+INSERT INTO bears (name, age) VALUES (NULL, 21);
