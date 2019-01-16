3e09996ea46d4b0cbe69ef79fc3047d1742581a1
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-01-16 12:25:15 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (age, gender) VALUES (3, 'm');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'm');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'f');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'f');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'f');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'f');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'm');
+INSERT INTO bears (name, age, gender) VALUES ("itsname", 3, 'm');
