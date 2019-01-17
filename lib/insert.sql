69c70c4f38e2069ad1fa84b9f5b1b8dab3f60e1c
John Oncher <johnoncher@gmail.com>
2019-01-17 11:26:10 -0500

    Create table, populate table, and select data from table

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 12,"Male", "Brown", "Calm", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Brown","Female", "Rowdy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "Female" , "Black", "Proud", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "Male", "White", "Dead", FALSE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6 , "Female", "White", "Sad", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "Female", "Green", "Giddy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "Male", "Purple", "Joyful", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, null, "Female", "Brown", null, TRUE);
